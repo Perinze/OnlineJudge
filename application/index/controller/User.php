24121a3d76ebe9ceed2a42a07d230c6de665bf30
DarkKris <long4664030@163.com>
2018-05-04 12:51:59 +0800

    新增User控制器

---
+++ application/index/controller/User.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DarkKris
+ * Date: 2018/5/4
+ * Time: 下午12:50
+ */
+namespace app\index\Controller;
+
+class User extends Base{
+    public function changeInfo()
+    {
+
+    }
+}
