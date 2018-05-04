ffa04067898b258177e84a8c6dca424ccdd7ecd2
DarkKris <long4664030@163.com>
2018-05-04 10:09:50 +0800

    新增registe方法与newRcode方法

--- application/index/controller/Register.php
+++ application/index/controller/Register.php
@@ -15,6 +15,42 @@ class Register extends Base{
             $ret = array('errCode'=>403,'errMsg'=>'token error','data'=>null);
             return json($ret);
         }
-        
+        $rcode = input('rcode');
+        $data = array(
+            'account' => input('post.account'),
+            'password' => input('post.password'),
+            'mail' => input('post.mail'),
+            'phone' => input('post.phone'),
+            'type' => $this->checkRcode($rcode)?0:1,
+        );
+        $userId = Db('user')->insertGetId($data);
+        if($userId)
+        {
+            $ret = array('errCode'=>0,'errMsg'=>'OK','data'=>null);
+            return json($ret);
+        }
+    }
+
+    public function newRcode($num)
+    {
+        if(!$this->checkToken(input('post.token')))
+        {
+            $ret = array('errCode'=>403,'errMsg'=>'token error','data'=>null);
+            return json($ret);
+        }
+
+        $i=1;
+        while($i<=$num)
+        {
+            $rcode = bin2hex(random_bytes(8));
+            $result = Db('rcode')->insert([
+                'code'=>$rcode,
+                'isUsed'=>false
+            ]);
+            if($result)$i++;
+        }
+
+        $ret = array('errCode'=>0,'errMsg'=>'OK','data'=>null);
+        return json($ret);
     }
 }
