724e764bc51e7cacfa8c7f6e86dc515866df302a
DarkKris <long4664030@163.com>
2018-05-04 12:45:56 +0800

    写入login方法

--- application/index/controller/Login.php
+++ application/index/controller/Login.php
@@ -9,6 +9,16 @@ namespace app\index\Controller;
 
 class Login extends Base{
     public function login(){
-
+        $ret = array('errCode'=>0,'errMsg'=>'OK','data'=>null);
+        $account = input('post.account');
+        $password = md5(base64_encode(input('post.password')));
+        $user = Db('user')->where(array('account'=>$account,'password'=>$password))->find();
+        if($user)
+        {
+            session('userId',$user['userId']);
+        }else{
+            $ret = array('errCode'=>102,'errMsg'=>'用户名或密码错误','data'=>null);
+        }
+        return json($ret);
     }
 }
