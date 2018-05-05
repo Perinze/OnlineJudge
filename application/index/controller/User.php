a1abf6c2e016d3b1597214d2cd8cccdb939c5eeb
DarkKris <long4664030@163.com>
2018-05-05 19:14:11 +0800

    添加changeType方法用于改变成员级别

--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -7,11 +7,41 @@
  */
 namespace app\index\Controller;
 
+use think\Log;
+
 require_once "UserValidate.php";
 
 class User extends Base{
     public function changeInfo()
     {
 
+    }
+
+    public function changeType($userId,$type)
+    {
+        if($this->checkToken(input('post.token'))!==true)
+        {
+            $ret = array('errCode'=>403,'errMsg'=>'token error','data'=>null);
+            return json($ret);
+        }
+
+        if($this->checkUserType(session('userId'))<=1)
+        {
+            $ret = array('errCode'=>403,'errMsg'=>'没有操作权限','data'=>null);
+            return json($ret);
+        }
+
+        $result = Db('user')->where('userId',$userId)->setField('type',$type);
+        if(!$result)
+        {
+            $ret = array('errCode'=>102,'errMsg'=>'修改失败','data'=>null);
+            return json($ret);
+        }else {
+            $time = date("Y-m-d H:i:s",time());
+            $sessionuid = session('userId');
+            Log::info("Opt {$time} [{$sessionuid}] Change User $userId");
+            $ret = array('errCode' => 0, 'errMsg' => 'OK', 'data' => null);
+            return json($ret);
+        }
     }
 }
