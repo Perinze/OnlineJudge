874e1610588dbdfe8353805414edd7a4c0682b6a
DarkKris <long4664030@163.com>
2018-08-20 00:22:48 +0800

    feat : 配置远程端数据库

--- application/panel/config/database.php
+++ application/panel/config/database.php
@@ -13,15 +13,15 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '115.159.77.141',
     // 数据库名
     'database'        => 'openjudge_db',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => 'root',
+    'password'        => 'acmWhut+123',
     // 端口
-    'hostport'        => '8889',
+    'hostport'        => '3306',
     // 连接dsn
     'dsn'             => '',
     // 数据库连接参数
