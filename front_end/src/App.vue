60b26cdce8d9567c4114853cc664f2f7d90a13e8
DarkKris <long4664030@163.com>
2019-03-09 13:59:45 +0800

    fix: re-init front_end to fix issue

--- front_end/src/App.vue
+++ front_end/src/App.vue
@@ -1,28 +1,147 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <a-layout id="components-layout-demo-custom-trigger" style="height: 100%">
+    <a-layout-sider
+            :trigger="null"
+            collapsible
+            v-model="collapsed"
+    >
+      <div class="logo" />
+      <a-menu theme="dark" mode="inline" :defaultSelectedKeys="['1']">
+        <a-menu-item key="1">
+          <a-icon type="user" />
+          <span>nav 1</span>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <a-icon type="video-camera" />
+          <span>nav 2</span>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <a-icon type="upload" />
+          <span>nav 3</span>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header style="background: #fff; padding: 0">
+        <a-icon
+                class="trigger"
+                :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+                @click="()=> collapsed = !collapsed"
+        />
+      </a-layout-header>
+      <a-layout-content :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '280px' }">
+        Content
+      </a-layout-content>
+    </a-layout>
+    <!--<a-layout>-->
+      <!--<a-layout-content>-->
+
+      <!--</a-layout-content>-->
+    <!--</a-layout>-->
+    <div>
+      <a-button type="primary" @click="showDrawer">
+        Open
+      </a-button>
+      <a-drawer
+              title="Multi-level drawer"
+              width=520
+              :closable="false"
+              @close="onClose"
+              :visible="visible"
+      >
+        <a-button type="primary" @click="showChildrenDrawer">
+          Two-level drawer
+        </a-button>
+        <a-drawer
+                title="Two-level Drawer"
+                width=320
+                :closable="false"
+                @close="onChildrenDrawerClose"
+                :visible="childrenDrawer"
+        >
+          <a-button type="primary" @click="showChildrenDrawer">
+            This is two-level drawer
+          </a-button>
+        </a-drawer>
+        <div
+                :style="{
+          position: 'absolute',
+          bottom: 0,
+          width: '100%',
+          borderTop: '1px solid #e8e8e8',
+          padding: '10px 16px',
+          textAlign: 'right',
+          left: 0,
+          background: '#fff',
+          borderRadius: '0 0 4px 4px',
+        }"
+        >
+          <a-button
+                  style="marginRight: 8px"
+                  @click="onClose"
+          >
+            Cancel
+          </a-button>
+          <a-button @click="onClose" type="primary">
+            Submit
+          </a-button>
+        </div>
+      </a-drawer>
+    </div>
+  </a-layout>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    export default {
+        data() {
+            return {
+                visible: false,
+                childrenDrawer: false
+            }
+        },
+        methods: {
+            showDrawer() {
+                this.visible = true
+            },
+            onClose() {
+                this.visible = false
+            },
+            showChildrenDrawer() {
+                this.childrenDrawer = true
+            },
+            onChildrenDrawerClose() {
+                this.childrenDrawer = false
+            },
+        },
+    }
+</script>
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+<script>
+    export default {
+        data(){
+            return {
+                collapsed: false,
+            }
+        },
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #components-layout-demo-custom-trigger .trigger {
+    font-size: 18px;
+    line-height: 64px;
+    padding: 0 24px;
+    cursor: pointer;
+    transition: color .3s;
+  }
+
+  #components-layout-demo-custom-trigger .trigger:hover {
+    color: #1890ff;
+  }
+
+  #components-layout-demo-custom-trigger .logo {
+    height: 32px;
+    background: rgba(255,255,255,.2);
+    margin: 16px;
+  }
 </style>
--- front_end/src/main.js
+++ front_end/src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
-import App from './App.vue'
-
+import Antd from 'ant-design-vue'
+import App from './App'
+import 'ant-design-vue/dist/antd.css'
 Vue.config.productionTip = false
 
+Vue.use(Antd)
+
+/* eslint-disable no-new */
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: h => h(App)
+}).$mount("#app")
+// new Vue({
+//     el: '#app',
+//     components: { App },
+//     template: '<App/>'
+// })
