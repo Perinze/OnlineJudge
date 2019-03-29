a70fd46558bf22d53069861a5d8f7ba867398cca
kdl12138 <ssbljw@163.com>
2019-03-29 20:20:44 +0800

    【feat】：添加获取统计数据接口

---
+++ back_end/application/oj/controller/Submit.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ASUS
+ * Date: 2019/3/29
+ * Time: 19:41
+ */
+
+namespace app\oj\controller;
+
+
+class Submit extends Base
+{
+
+}
---
+++ back_end/application/oj/model/SubmitModel.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ASUS
+ * Date: 2019/3/29
+ * Time: 19:42
+ */
+
+namespace app\oj\model;
+
+
+use think\Model;
+
+class SubmitModel extends Model
+{
+
+}
