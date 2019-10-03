234d7212e1f52ea9050bc9a7448aa01a6c85d8a5
DarkKris <long4664030@163.com>
2019-10-03 20:46:39 +0800

    feat: 添加路由

--- front_end/src/router/router.js
+++ front_end/src/router/router.js
@@ -7,37 +7,73 @@ import contestlist from '../page/contestlist';
 import problemdetail from '../page/tempProblemDetail';
 import contestrank from '../page/contest-rank';
 import contestpage from '../page/contestpage';
+import discusslist from '../page/discusslist';
+import discussdetail from '../page/discussdetail';
+import submitpage from '../page/submitpage';
+import page404 from '../page/page404';
 
 Vue.use(VueRouter);
 
 const routes = [
     {
+        // 重定向到主页
         path: '',
         redirect: '/main'
     },
     {
+        // 主页
         path: '/main',
         component: mainpage
     },
     {
+        // 题目列表
         path: '/problem',
         component: problemlist
     },
     {
+        // 题目详情
         path: '/problem/:id',
         component: problemdetail
     },
     {
+        // 比赛列表
         path: '/contest',
         component: contestlist
     },
     {
+        // 比赛主页
         path: '/contest/:id',
         component: contestpage
     },
     {
+        // 比赛排行榜
         path: '/rank/:id',
         component: contestrank
+    },
+    {
+        // (某场比赛)讨论板列表
+        path: '/discuss/:id',
+        component: discusslist
+    },
+    {
+        // (某场比赛)讨论板单条详情
+        path: '/discuss/:id/:did',
+        component: discussdetail
+    },
+    {
+        //  提交页面 redirect
+        path: '/submit',
+        redirect: '/submit/1000'
+    },
+    {
+        // 提交页面
+        path: '/submit/:pid',
+        component: submitpage
+    },
+    {
+        // default
+        path: '*',
+        component: page404
     }
 ];
 
