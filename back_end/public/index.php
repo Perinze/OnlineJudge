cd74e2f33e7e3f5f5ccda34b947afcef5081e29d
DarkKris <long4664030@163.com>
2019-10-05 16:56:53 +0800

    【fix】：尝试跨域

--- back_end/public/index.php
+++ back_end/public/index.php
@@ -14,9 +14,13 @@ namespace think;
 
 // 加载基础文件
 require __DIR__ . '/../thinkphp/base.php';
-header("Access-Control-Allow-Origin:*");
-header("Access-Control-Allow-Methods:GET, POST, OPTIONS, DELETE");
-header("Access-Control-Allow-Headers:DNT,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type, Accept-Language, Origin, Accept-Encoding");
+
+if($_SERVER['REQUEST_METHOD'] == 'OPTIONS'){
+		header("Access-Control-Allow-Origin: *");
+		header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
+		header('Access-Control-Allow-Methods: GET, POST, PUT,DELETE,OPTIONS,PATCH');
+    exit;
+}
 // 支持事先使用静态方法设置Request对象和Config对象
 
 // 执行应用并响应
