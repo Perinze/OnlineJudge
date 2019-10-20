69af4536f4ba25a81a80c75ed7cde01db4f4d4e4
DarkKris <long4664030@163.com>
2019-10-20 15:43:58 +0800

    feat: 区分开发环境、生产环境

--- front_end/vue.config.js
+++ front_end/vue.config.js
@@ -1,4 +1,4 @@
-// const webpack = require('webpack')
+const webpack = require('webpack');
 const CompressionPlugin = require("compression-webpack-plugin");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const StatsPlugin = require('stats-webpack-plugin');
@@ -9,32 +9,43 @@ module.exports = {
 		sourceMap: false,
 	},
 
-    configureWebpack: {
-      plugins: [
-          new StatsPlugin('stats.json', {
-            chunkModules: true,
-            chunks: true,
-            assets: false, 
-            modules: true,
-            children: true,
-            chunksSort: true, 
-            assetsSort: true
-          }),
-          new CompressionPlugin({
-              test:/.(js|css|svg|woff|ttf|json|html|otf)$/,
-              threshold:10240,
-              deleteOriginalAssets:false,
-          }),
-          new UglifyJsPlugin({
-              uglifyOptions: {
-                  compress: {
-                      drop_debugger: true,
-                      drop_console: true
-                  }
-              },
-              sourceMap: false,
-              parallel: true
-          })
-      ]
+    configureWebpack: config => {
+        let devPlugin = [
+        	new webpack.HotModuleReplacementPlugin(),
+	        new StatsPlugin('stats.json', {
+	            chunkModules: true,
+	            chunks: true,
+	            assets: false,
+	            modules: true,
+	            children: true,
+	            chunksSort: true,
+	            assetsSort: true
+	        }),
+        ];
+        let proPlugin = [
+        	new webpack.HotModuleReplacementPlugin(),
+	        new CompressionPlugin({
+				test:/.(js|css|svg|woff|ttf|json|html|otf)$/,
+				threshold:10240,
+				deleteOriginalAssets:false,
+			}),
+			new UglifyJsPlugin({
+				uglifyOptions: {
+					compress: {
+						drop_debugger: true,
+						drop_console: true
+					}},
+				sourceMap: false,
+				parallel: true
+			})
+		]
+    	if(process.env.NODE_ENV === 'production') {
+    		// 生产环境
+			config.plugins = [...config.plugins, ...proPlugin];
+    	}else{
+    		// 开发环境
+			config.plugins = [...config.plugins, ...devPlugin];
+    	}
+      
     },
 }
