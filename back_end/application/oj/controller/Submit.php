c71efd5648ce5a312ad4d4ff743c9245f0ea6dd1
kdl12138 <ssbljw@163.com>
2019-11-14 14:10:18 +0800

    【update】：修改交题时间限制为1s

--- back_end/application/oj/controller/Submit.php
+++ back_end/application/oj/controller/Submit.php
@@ -108,8 +108,8 @@ class Submit extends Base
         }
         $contest_id = 0;
         $temp = $submit_model->get_the_submit(['submit.user_id' => $user_id]);
-        if(!empty($temp['data']) && $time - strtotime($temp['data'][count($temp['data']) - 1]['submit_time']) < 5){
-            return apiReturn(CODE_ERROR, '请5S后再交题');
+        if(!empty($temp['data']) && $time - strtotime($temp['data'][count($temp['data']) - 1]['submit_time']) < 1){
+            return apiReturn(CODE_ERROR, '请1S后再交题');
         }
         if ($problem['data']['status'] === CONTEST) {
             if (isset($req['contest_id'])) {
