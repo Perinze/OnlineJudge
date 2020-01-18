2f41d6c768f58ed64cde216f503213b5c87cd50c
lxh027 <992874705@qq.com>
2020-01-18 16:44:21 +0800

    add: 权限管理后台页面

--- back_end/application/panel/controller/Role.php
+++ back_end/application/panel/controller/Role.php
@@ -8,16 +8,109 @@ use app\panel\model\UserModel;
 use think\Controller;
 
 class Role extends Base {
-    public function test() {
-        $model = new RoleGroupModel();
-        $authModel = new AuthorityModel();
-        $userModel = new UserModel();
-        $data = [
-            'nick' => 'lxh001',
-            'role_group' => ['super'],
-            'group_name' => ['']
+
+    /**
+     * 获取所有权限组
+     */
+    public function getAllRoleGroup() {
+        $role_group_model = new RoleGroupModel();
+        $req = input('post.aoData');
+        $where = aoDataFormat($req, 'group_name');
+        $resp = $role_group_model->getAllGroup();
+        echo datatable_response($resp['code'], $where['where'], $resp['data'], $role_group_model);
+    }
+
+    /*
+     * 添加权限组
+     */
+    public function addRoleGroup() {
+        $group_name = input('get.group_name');
+        $role_group_model = new RoleGroupModel();
+        $msg = $role_group_model->addRoleGroup(['group_name' =>$group_name]);
+        return apiReturn($msg['code'], $msg['msg'], $msg['data'], 200);
+    }
+
+    /*
+     * 删除权限组
+     */
+    public function deleteRoleGroup() {
+        $group_name = input('get.group_name');
+        $role_group_model = new RoleGroupModel();
+        $role_group_model->deleteRoleGroup(['group_name' =>$group_name]);
+        $this->redirect("panel/role/index");
+    }
+    /*
+     * 更新权限组
+     */
+    public function updateRoleGroup() {
+        $group_name = input('post.group_name');
+        $authorities = input('post.authorities');
+        $role_group_model = new RoleGroupModel();
+        $where = [
+            "group_name" => $group_name,
+            "authority" => (array)$authorities
         ];
-        $result = $authModel->addAuthority(['name'=>'add']);
-        return apiReturn(200, 'OK', $result);
+        $msg = $role_group_model->updateAuthority($where);
+        return apiReturn($msg['code'], $msg['msg'], $msg['code'], 200);
+    }
+
+    /*
+     * 获取所有权限
+     */
+    public function getAllAuthority() {
+        $authority_model = new AuthorityModel();
+        $req = input('post.aoData');
+        $where = aoDataFormat($req, 'name');
+        $resp = $authority_model->getAllAuthority();
+        echo datatable_response($resp['code'], $where['where'], $resp['data'], $authority_model);
+    }
+
+    public function addAuthority() {
+        $auth = input('get.auth');
+        $authority_model = new AuthorityModel();
+        $msg = $authority_model->addAuthority(['name' => $auth]);
+        return apiReturn($msg['code'], $msg['msg'], $msg['data'], 200);
+    }
+
+    public function deleteAuthority() {
+        $auth = input('get.auth');
+        $authority_model = new AuthorityModel();
+        $msg = $authority_model->deleteAuthority($auth);
+        $this->redirect("panel/role/auth");
+    }
+
+    public function switchAuthorityStatus() {
+        $auth = input('get.auth');
+        $authority_model = new AuthorityModel();
+        $msg = $authority_model->switchAuthority($auth);
+        $this->redirect("panel/role/auth");
+    }
+
+    public function edit() {
+        $group_name = input('get.group_name');
+        $role_group_model = new RoleGroupModel();
+        $authority_model = new AuthorityModel();
+        $data = $role_group_model->getRoleGroup($group_name)['data'];
+        $authorities = $authority_model->getAuthorities(['enabled' => 1])['data'];
+        foreach ($authorities as $authority) {
+            if (in_array($authority['name'], $data['authority']->auth)) {
+                $authority['checked'] = 'checked';
+            } else {
+                $authority['checked'] = '';
+            }
+        }
+        $returnData = [
+            'name' => $data['group_name'],
+            'auth' => $authorities
+        ];
+        return $this->fetch('', $returnData);
+    }
+
+    public function auth() {
+        return $this->fetch();
+    }
+
+    public function index() {
+        return $this->fetch();
     }
 }
