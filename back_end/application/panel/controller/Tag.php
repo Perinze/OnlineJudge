d1d54febd6927629e986f80430bb80f1135c4267
kdl12138 <949257748@qq.com>
2020-02-01 18:17:18 +0800

    【feat】：完成标签页面

--- back_end/application/panel/controller/Tag.php
+++ back_end/application/panel/controller/Tag.php
@@ -20,22 +20,46 @@ class Tag extends Base
 
     public function getTheTag()
     {
-
+        $tag_model = new TagModel();
+        $req = input('post.');
+        if(!isset($req['id'])){
+            return apiReturn(CODE_ERROR, '请填写id', '');
+        }
+        $resp = $tag_model->getTheTag($req['id']);
+        return apiReturn($resp['code'], $resp['msg'], $resp['data']);
     }
 
     public function addTag()
     {
-
+        $tag_model = new TagModel();
+        $req = input('post.');
+        if(!isset($req['name'])){
+            return apiReturn(CODE_ERROR, '请填写标签名字', '');
+        }
+        $resp = $tag_model->addTag($req);
+        return apiReturn($resp['code'], $resp['msg'], $resp['data']);
     }
 
     public function editTag()
     {
-
+        $tag_model = new TagModel();
+        $req = input('post.');
+        if(!isset($req['id'])){
+            return apiReturn(CODE_ERROR, '请填写标签序号', '');
+        }
+        $resp = $tag_model->editTag($req);
+        return apiReturn($resp['code'], $resp['msg'], $resp['data']);
     }
 
     public function deleTag()
     {
-
+        $tag_model = new TagModel();
+        $req = input('post.');
+        if(!isset($req['id'])){
+            return apiReturn(CODE_ERROR, '请填写标签序号', '');
+        }
+        $resp = $tag_model->deleTag($req['id']);
+        return apiReturn($resp['code'], $resp['msg'], $resp['data']);
     }
 
     /* 页面 */
@@ -46,6 +70,9 @@ class Tag extends Base
 
     public function info()
     {
+        $req = input('get.');
+        $id = isset($req['id']) ? $req['id'] : 0;
+        $this->assign('id', $id);
         return $this->fetch();
     }
 }
