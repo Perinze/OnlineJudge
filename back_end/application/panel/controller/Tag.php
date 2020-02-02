568bf7c7e556be55114f2e8f0ecff4046acd58b5
kdl12138 <949257748@qq.com>
2020-02-02 22:36:16 +0800

    【fix】：修复bug，添加删除标签按钮

--- back_end/application/panel/controller/Tag.php
+++ back_end/application/panel/controller/Tag.php
@@ -18,6 +18,12 @@ class Tag extends Base
         echo datatable_response($resp['code'], $where['where'], $resp['data'], $tag_model);
     }
 
+    public function getAllTags()
+    {
+        $tag_model = new TagModel();
+        $resp = $tag_model->getAllTag(['status' => 1], 100, 0);
+        return apiReturn($resp['code'], $resp['msg'], $resp['data']);
+    }
     public function getTheTag()
     {
         $tag_model = new TagModel();
