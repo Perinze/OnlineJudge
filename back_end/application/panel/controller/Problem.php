e040189bf6ba95d42934d67d3062680d47426ea0
lxh027 <992874705@qq.com>
2020-03-22 21:43:38 +0800

    feat: 上传数据

--- back_end/application/panel/controller/Problem.php
+++ back_end/application/panel/controller/Problem.php
@@ -1,10 +1,12 @@
 <?php
 namespace app\panel\controller;
+use app\oj\model\CommonModel;
 use app\panel\model\ProblemModel;
 use app\oj\validate\ProblemValidate;
 
 class problem extends Base
 {
+    private $data_path = '../../../../../root/FinalRank/FinalRank/data/';
     /* 接口 */
     /**
      * 新建题目
@@ -102,12 +104,96 @@ class problem extends Base
         return apiReturn($resp['code'], $resp['msg'], $resp['data']);
     }
 
+    /**
+     * 上传题目数据接口, 支持多组数据
+     */
+    public function upload_data_file()
+    {
+        $files = request()->file('');
+        $req = input('post.');
+        $data = [];
+        foreach($files['file'] as $file) {
+            //halt($file);
+            $info = $file->move($this->data_path);
+            if ($info != VALIDATE_PASS) {
+                return apiReturn(CODE_ERROR, $file->getError(), '');
+            } else {
+                $file_path = $this->data_path . (str_replace('\\', '/', $info->getSaveName()));
+                $str = file_get_contents($file_path);
+                //$str = str_replace("\r\n", '\n', $str);
+                $filename = substr($file->getInfo()['name'], 0, strpos($file->getInfo()['name'], '.'));
+                if (!isset($data[$filename])) {
+                    if ($info->getExtension() === 'in') {
+                        $data[$filename] = array(
+                            'in' => $str,
+                            'out' => ''
+                        );
+                    } else {
+                        $data[$filename] = array(
+                            'in' => '',
+                            'out' => $str
+                        );
+                    }
+                } else {
+                    if ($info->getExtension() === 'in') {
+                        $data[$filename]['in'] = $str;
+                    } else {
+                        $data[$filename]['out'] = $str;
+                    }
+                }
+            }
+        }
+
+        if(!isset($req['sqj'])){
+            $re_data = array(
+                'type' => 'Normal',
+                'time_limit' => isset($req['time']) ? $req['time'] : 1000000000,
+                'memory_limit' => isset($req['memory']) ? $req['memory'] : 33554432,
+                'test_cases' => array()
+            );
+        } else {
+            $re_data = array(
+                'type' => 'Special',
+                'time_limit' => isset($req['time']) ? $req['time'] : 1000000000,
+                'memory_limit' => isset($req['memory']) ? $req['memory'] : 33554432,
+                'spj' => array(
+                    'language' => $req['language'],
+                    'code' => $req['code']
+                ),
+                'test_cases' => array()
+            );
+        }
+        foreach ($data as $item){
+            $re_data['test_cases'][] = array(
+                'input' => $item['in'],
+                'answer' => $item['out'],
+            );
+        }
+
+        // 数据文件输出
+        $json_data = json_encode($re_data);
+        $json_data = str_replace('\r\n', '\n', $json_data);
+        file_put_contents($this->data_path . $req['problem_id'] . '.json', $json_data);
+
+    }
+
     /* 页面 */
     /**
      * 添加题目页面
      */
     public function add()
     {
+        return $this->fetch();
+    }
+
+    /**
+     * 添加数据
+     */
+    public function data()
+    {
+        $req = input('get.');
+        $problem_id = isset($req['id']) ? $req['id'] : 0;
+        $this->assign('problem_id', $problem_id);
         return $this->fetch();
     }
 
