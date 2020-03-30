e826cb32789f1a2db1308a7f758a19914a69e45a
lxh027 <992874705@qq.com>
2020-03-30 13:31:37 +0800

    fix: 上传数据spj处理的问题

--- back_end/application/panel/controller/Problem.php
+++ back_end/application/panel/controller/Problem.php
@@ -171,7 +171,7 @@ class problem extends Base
         $toml = new TomlBuilder();
         $secs = isset($req['secs']) ? $req['secs'] : 1;
         $nanos = isset($req['nanos']) ? $req['nanos'] * 100000000 : 0;
-        if(!isset($req['spj'])){
+        if(isset($req['spj'])){
             // extern_problem文件夹创建
             $spj_toml = new TomlBuilder();
             $spj_path = $problem_path.'extern_program';
