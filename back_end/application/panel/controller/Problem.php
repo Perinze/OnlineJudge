dc0b402927eae65a850c248d63c0b300ad4841f6
kdl12138 <949257748@qq.com>
2020-05-05 22:07:31 +0800

    【fix】：改回来

--- back_end/application/panel/controller/Problem.php
+++ back_end/application/panel/controller/Problem.php
@@ -7,7 +7,7 @@ use Yosymfony\Toml\TomlBuilder;
 
 class problem extends Base
 {
-    private $data_path = '/home/oj_data/';
+    private $data_path = '../finalrank/data/';
     /* 接口 */
     /**
      * 新建题目
