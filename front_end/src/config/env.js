4e1c8a91691cb8921c830cb5f63a24bb896d3cf7
deantiwang <long4664030@163.com>
2020-09-06 21:01:43 +0800

    lint

--- front_end/src/config/env.js
+++ front_end/src/config/env.js
@@ -2,21 +2,17 @@
  * 配置环境文件
  */
 
-
 /**
  * baseUrl: 请求域名前缀
  */
 
-let baseUrl = 'http://acmwhut.com/api/oj/';
+let baseUrl = "http://acmwhut.com/api/oj/";
 
 /**
  * 百度统计
  * baidu_site_id: 站点id
  */
 
-let baidu_site_id = '14001473';
+let baidu_site_id = "14001473";
 
-export {
-    baseUrl,
-    baidu_site_id,
-}
+export { baseUrl, baidu_site_id };
--- front_end/vue.config.js
+++ front_end/vue.config.js
@@ -1,50 +1,48 @@
-const webpack = require('webpack');
+const webpack = require("webpack");
 const CompressionPlugin = require("compression-webpack-plugin");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-const StatsPlugin = require('stats-webpack-plugin');
+const StatsPlugin = require("stats-webpack-plugin");
 module.exports = {
-	productionSourceMap: false,
+  productionSourceMap: false,
+  css: {
+    sourceMap: false,
+  },
+  configureWebpack: (config) => {
+    let proPlugin = [
+      new CompressionPlugin({
+        test: /.(js|css|svg|woff|ttf|json|html|otf)$/,
+        threshold: 10240,
+        deleteOriginalAssets: false,
+      }),
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          compress: {
+            drop_debugger: true,
+            drop_console: true,
+          },
+        },
+        sourceMap: false,
+        parallel: true,
+      }),
+      new StatsPlugin("stats.json", {
+        chunkModules: true,
+        chunks: true,
+        assets: false,
+        modules: true,
+        children: true,
+        chunksSort: true,
+        assetsSort: true,
+      }),
+    ];
 
-	css: {
-		sourceMap: false,
-	},
+    let devPlugin = [new webpack.HotModuleReplacementPlugin()];
 
-    configureWebpack: config => {
-        let proPlugin = [
-	        new CompressionPlugin({
-				test:/.(js|css|svg|woff|ttf|json|html|otf)$/,
-				threshold:10240,
-				deleteOriginalAssets:false,
-			}),
-			new UglifyJsPlugin({
-				uglifyOptions: {
-					compress: {
-						drop_debugger: true,
-						drop_console: true
-					}},
-				sourceMap: false,
-				parallel: true
-			}),
-			new StatsPlugin('stats.json', {
-	            chunkModules: true,
-	            chunks: true,
-	            assets: false,
-	            modules: true,
-	            children: true,
-	            chunksSort: true,
-	            assetsSort: true
-	        })
-		];
-		let devPlugin = [
-        	new webpack.HotModuleReplacementPlugin(),
-		]
-    	if(process.env.NODE_ENV === 'test') {
-    		// 开发环境
-    		config.plugins = [...config.plugins, ...devPlugin];
-    	}else{
-    		// 生产环境
-			config.plugins = [...config.plugins, ...proPlugin];
-    	}
-      
-    },
-}
+    if (process.env.NODE_ENV === "test") {
+      // 开发环境
+      config.plugins = [...config.plugins, ...devPlugin];
+    } else {
+      // 生产环境
+      config.plugins = [...config.plugins, ...proPlugin];
+    }
+  },
+};
