3bbbcff97eb5427d30f0089c89d28f59676d1366
deantiwang <long4664030@163.com>
2020-10-31 16:42:27 +0800

    feat: 开放java与python

--- front_end/src/components/top-drawer.vue
+++ front_end/src/components/top-drawer.vue
@@ -59,16 +59,16 @@ export default {
           value: "cpp.g++",
           cmValue: "text/x-c++src",
         },
-        // {
-        //     name: 'Java',
-        //     value: 'java',
-        //     cmValue: 'text/x-java'
-        // },
-        // {
-        //     name: 'Python3.6',
-        //     value: 'python',
-        //     cmValue: 'python'
-        // },
+        {
+            name: 'Java',
+            value: 'java.openjdk10',
+            cmValue: 'text/x-java'
+        },
+        {
+            name: 'Python3.6',
+            value: 'python.cpython3.6',
+            cmValue: 'python'
+        },
       ],
     };
   },
