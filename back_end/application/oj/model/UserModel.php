71b5b4a29e2c77cfec01c3935014bd7569f0da62
LAPTOP-LCL90KNN\kdl12138 <ssbljw@163.com>
2020-11-02 21:33:39 +0800

    [fix]: ac cnt distinct

--- back_end/application/oj/model/UserModel.php
+++ back_end/application/oj/model/UserModel.php
@@ -90,6 +90,11 @@ class UserModel extends Model
                     ->where('user_id', $res['user_id'])
                     ->group('status')
                     ->select();
+                $res['all_problems']['AC'] = Db::table('submit')
+                    ->where('user_id', $res['user_id'])
+                    ->where('status', 'AC')
+                    ->group('problem_id')
+                    ->count();
                 return ['code' => CODE_SUCCESS, 'msg' => '登陆成功', 'data' => $res];
             }
             return ['code' => CODE_ERROR, 'msg' => '用户名或密码错误', 'data' => ''];
