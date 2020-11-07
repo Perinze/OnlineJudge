0516708ed261eac982f5f406adc3f4926f059145
afwe <936017948@qq.com>
2020-11-07 09:07:28 +0800

    notify v 0.1

--- front_end/src/plugins/notify/notify.vue
+++ front_end/src/plugins/notify/notify.vue
@@ -1,45 +1,44 @@
-<template>
-</template>
+<template></template>
 <script>
-import getNotification from '.../api/getData.js'
+import getNotification from ".../api/getData.js";
 export default {
   name: "notify",
-  props:{
-    cid:String
+  props: {
+    cid: String,
   },
   data() {
     return {
-      iswork:true,
-      notice:'nothing'
-    }
+      iswork: true,
+      notice: "nothing",
+    };
   },
-  watch:{
-    notice(newval){
+  watch: {
+    notice(newval) {
       this.$notify({
-      title:'比赛通知',
-      message:newval,
-      duration:0
-      })
-    }
+        title: "比赛通知",
+        message: newval,
+        duration: 0,
+      });
+    },
   },
   created() {
     this.intervalNotify();
   },
   methods: {
     intervalNotify() {
-      let self=this;
-      let interval=setInterval(()=>{ 
-        if(this.iswork){
-        getNotificationByID(cid).then((response)=>{
-            if(reseponse.status==0){
-              self.notice=response.data.content;
+      let self = this;
+      let interval = setInterval(() => {
+        if (this.iswork) {
+          getNotificationByID(cid).then((response) => {
+            if (reseponse.status == 0) {
+              self.notice = response.data.content;
             }
-        })
-        } else{
+          });
+        } else {
           clearInterval(interval);
         }
-        },5000)
-      }
-  }
-}
+      }, 5000);
+    },
+  },
+};
 </script>
