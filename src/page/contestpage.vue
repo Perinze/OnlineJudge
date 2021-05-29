df6a711db6b01912ef2109bb3cf612d97d9ebd9b
deantiwang <long4664030@163.com>
2021-05-29 22:28:19 +0800

    fix: 这波我全锅

--- src/page/contestpage.vue
+++ src/page/contestpage.vue
@@ -92,7 +92,7 @@
                 :key="'preblem-self'+index"
                 class="problem-self"
                 @click="callProblem(contest_info.problems[index - 1], contest_info.id)">
-              {{ `${String.fromCharCode(64 + index)} ${contest_info.problemInfo.title || ""}` }}  
+              {{ `${String.fromCharCode(64 + index)} ${contest_info.problemInfo[index - 1].title || ""}` }}  
             </div>
           </li>
         </ol>
